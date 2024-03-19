524b68eeb899f03150ee2038f643ec8a1b8ddd7c
Helbert <helbertsousar@gmail.com>
2024-03-19 18:12:50 -0300

    refactor: updating settings and folders

---
+++ BlazorCrudApp/BlazorCrudApp/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using BlazorCrudApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrudApp.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
